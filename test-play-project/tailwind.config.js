6018e3a1e2cacf6a78313f8e1824cd05b8cba768
Tanin Na Nakorn <@tanin>
2024-05-24 22:10:52 -0700

    Integrate with sbt-postcss

---
+++ test-play-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './public/**/*.{html,js,css}',
+    './app/**/*.{html,ts,js,svelte,css,scss}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
